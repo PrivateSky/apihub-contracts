ff86feca034a07c204c67b2fe9ce2bef8810f438
Alboaie Sînică <salboaie@gmail.com>
2021-05-24 18:03:01 +0300

    Update code.js

--- code/bdns/code.js
+++ code/bdns/code.js
@@ -1,6 +1,29 @@
 /*
+
+ local (poate fi apelat numai din alt smart contract si nu din APIHub)
+ read (fara semnatura si consens, nu se inregistreaza)
+ write (consens)
+ describeMethods(){
+   read:[m1,m2]
+   write:["m3"]   
+ }
+
+ BDNS Smart Contract
   addSubdomain(controllerDID, signature, callback)
   getSubdomainInfo(callback)
   updateSubdomainInfo(signature, JSON, callback)
   deleteSubdomain(signature)
+
+ApiHub Endpoints
+/generateWriteCommand ({contract, method,args:[args...],nounce, signature})  //generateReadCommand(Consensus, getNounce, signedDID) 
+/generateReadCommand({contract, method,args:[args...]})
+
+Consensus Smart Contract
+  getNonce(SignerDID)
+  sendCommandToConsensus(command, callback)
+  ..
+  
+  
 */
+
+
